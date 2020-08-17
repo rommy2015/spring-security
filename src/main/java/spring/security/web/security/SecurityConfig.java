77d4425bd4374b9a2de0d7fb739eea522e5b3bf9
valery <skyho@yandex.ru>
2020-08-17 11:20:16 +0300

    редактирование README.md

--- src/main/java/spring/security/web/security/SecurityConfig.java
+++ src/main/java/spring/security/web/security/SecurityConfig.java
@@ -6,14 +6,35 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+
 @Configuration
-public class SecurityConfig {
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(SecurityConfig.class);
 
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+             http
+                .authorizeRequests() /*все запросы должны проходить авторизацию*/
+                      /*запросы, на адреса, совпдающие с представленным шаблоном, не будут авторизовываться*/
+                .antMatchers("/css/**", "/js/**").permitAll()
+                /*Все остальные запросы, должны быть от пользователя, который прошел аутентификацию*/
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().permitAll() /*страница для ввода данных учетной записи, открыта для всех*/
+                .and()
+                .logout().permitAll();/*страница для выхода из сеанса, открыта для всех*/
+
+    }
 
     /**
      * `AuthenticationManagerBuilder` позволяет легко построить в памяти аутентификацию,
@@ -21,10 +42,11 @@ public class SecurityConfig {
      * добавляя `AuthenticationProvider`.
      * inMemoryAuthentication() - Метод орагнизовывает аутентификацию пользователей, данные
      * о которой храняться в памяти приложения
+     *
      * @param auth
      */
     @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth)  {
+    public void configureGlobal(AuthenticationManagerBuilder auth) {
 
         String password = passwordEncoder().encode("user");
 
