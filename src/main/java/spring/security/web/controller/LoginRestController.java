7c242b3c6f03bca7dc6a93949e4e34fe2fd0e8f0
valery <skyho@yandex.ru>
2020-08-20 09:41:20 +0300

    сделан доступ к пользовательской форме для входа в систему.

--- src/main/java/spring/security/web/controller/LoginRestController.java
+++ src/main/java/spring/security/web/controller/LoginRestController.java
@@ -1,16 +1,14 @@
-package spring.security.web.rest;
+package spring.security.web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class LoginRestController {
 
-    @GetMapping("/loginCustom")
+    @GetMapping("/login")
     public String getLogin(
             @RequestParam (value = "error", required = false) String error,
             @RequestParam (value = "logout", required = false) String logout,
@@ -19,6 +17,6 @@ public class LoginRestController {
         model.addAttribute("error", error != null);
         model.addAttribute("logout", logout != null);
 
-        return "login.ftl";
+        return "login";
     }
 }
